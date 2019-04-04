b443073fe2333f50f3cf45272395e2dcb427462c
Imane219 <u201813486@hust.edu.cn>
2019-04-04 17:58:42 +0800

    commit

--- chapter3/3.21.c
+++ chapter3/3.21.c
@@ -12,6 +12,12 @@
     5.a走到了末尾，将栈内剩余运算符pop存入b
 
 */
+/*debug辛酸史：
+	1.initstack 不能在里面开stack然后把地址传出去，因为函数调用完分配的空间就没了啊
+	2.b没有全部置0
+	3.编译器出了一些乱七八糟的符号，指针问题
+	
+*/ 
 
 
 #include <stdio.h>
@@ -27,19 +33,6 @@ typedef struct stack
     char * top;
 }stack,* pstack;
 
-pstack initstack()
-{
-    stack op;
-    pstack s = &op;
-    op.base = (char*)malloc(N*sizeof(char));
-    op.top = op.base;
-    if(op.base == NULL)
-    {
-        printf("no enough memory");
-        exit(1);
-    }
-    return s;
-}
 
 void push(pstack s,char x)
 {
@@ -68,32 +61,30 @@ char gettop(pstack s)
 char compare(pstack s,char x)
 {
     if(gettop(s) =='0'&&(x<'a'||x>'z'))
-        return '3';
-    /*else if(x == '+'||x == '-')
-        return '+';
+        return '>';
+    else if(x == '+'||x == '-')
+        return '<';
     else if((x == '*'||x == '/')&&(gettop(s) == '+'||gettop(s) == '-'))
         return '>';
     else if((x == '*'||x == '/')&&(gettop(s) == '*'||gettop(s) == '/'))
         return '<';
     else return '0';
-*/
+
 }
 
 int main()
 {
-    //char a[N] = {"-"};
-    //char b[N]= {0};
-    //char *pb = b;
-    pstack s = initstack();
-    //char *p = a;
+	stack op;
+    pstack s = &op;
+    op.base = (char*)malloc(N*sizeof(char));
+    op.top = op.base;
+    
+    char a[N] = {"a+b/c-d+e"};
+    char b[N]= {0};//最好将b置0 
+    char *pb = b;
+    char *p = a;
 
-	char m = '*';
-	if(gettop(s) =='0'&&(m<'a'||m>'z'))
-		printf("%c",'=');
-    char x = compare(s,'*');
-    printf("%c",x);
-
-    /*for(;*p;p++)
+    for(;*p;p++)
     {
         switch(compare(s,*p))
         {
@@ -105,5 +96,5 @@ int main()
 
     for(;s->top!=s->base;pb++)pop(s,pb);
     printf("%s",b);
-    */return 0;
+    return 0;
 }
